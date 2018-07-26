c85b1535b541b5598cdd0ea574985c7ff03e970a
QuentinDll <quentin.delplace13@gmail.com>
2018-07-26 16:20:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 4</title>
+</head>
+<body>
+<p>
+<?php
+$string = 'Yo';
+$int = 21;
+$float = 12.12;
+$bool = (boolean) true;
+echo $string;
+echo $int;
+echo $float;
+echo $bool;
+ ?>
+</p>
+</body>
+</html>
